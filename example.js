3a1375c9d2ade7db16976493851396b6a87573fa
Arnis Vuskans <ampatspell@gmail.com>
2020-08-27 17:51:13 +0300

    example

---
+++ example.js
@@ -0,0 +1,20 @@
+let gs = require('./index');
+
+(async () => {
+
+
+  let result = await gs()
+    .batch()
+    .nopause()
+    .page(5)
+    .textAlphaBits(4)
+    .graphicsAlphaBits(4)
+    .interpolate()
+    .device('png16m')
+    .output('output.png')
+    .input('input.pdf')
+    .exec();
+
+  console.log(result);
+
+})().then(() => {}, err => console.log(err));
